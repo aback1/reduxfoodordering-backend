37319ce07958306e9d8cfddc5f1372600d1404fa
aback <162435971+aback1@users.noreply.github.com>
2025-01-31 19:31:32 +0100

    Update AuthenticationController.php

--- src/Controller/AuthenticationController.php
+++ src/Controller/AuthenticationController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Repository\UserRepository; // Correctly reference the UserRepository
+use App\Repository\UserRepository; 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
